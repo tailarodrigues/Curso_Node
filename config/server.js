a9bdd548bcfb26b3686dfe9e6222605490919a08
Taíla Rodrigues <tailarodrigues_bd@yahoo.com.br>
2017-03-29 14:36:57 -0300

    Dados via POST
    Recebendo os dados dos formularios via POST com Body-Parser

--- config/server.js
+++ config/server.js
@@ -2,13 +2,13 @@ var express  = require('express');
 
 //para que não precise declarar todos os diretorios no arquivo app.js
 var consign = require('consign');
-
+var bodyParser = require('body-parser');
 
 var app = express();
 	app.set('view engine', 'ejs');
-
 	app.set('views','./app/views');
 
+app.use(bodyParser.urlencoded({extended: true}));
 
 consign()
 .include('app/routes') //o consign me possibilita incluir mais rotas, lembrnado que preciso colocar a extensão do arquivo.
