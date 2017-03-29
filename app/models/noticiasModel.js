24da1b817092ef060bfab70e38cc64fd0782414c
Taíla Rodrigues <tailarodrigues_bd@yahoo.com.br>
2017-03-29 16:22:19 -0300

    Adaptação para comando INSERT BD
    Implementação para comando INSERT BD

--- app/models/noticiasModel.js
+++ app/models/noticiasModel.js
@@ -8,6 +8,10 @@ module.exports = function(){
 		connection.query('SELECT * FROM noticias WHERE id_noticia = 2', callback);
 	}	
 
+	this.salvarNoticia = function(noticias, connection, callback){
+		connection.query('INSERT INTO noticias SET ?', noticias, callback);
+	}
+
 	return this;
 
 }
--- app/routes/admin.js
+++ app/routes/admin.js
@@ -7,7 +7,16 @@ module.exports = function(app){
 	app.post('/noticias/salvar', function(req, res){
 		var noticias = req.body;
 
-		res.send(noticias);
+		var connection = app.config.dbConnection();
+		var noticiasModel = app.app.models.noticiasModel;
+
+		noticiasModel.salvarNoticia(noticias, connection, function(error, result){
+			//redirect esta voltando a pagina para alguma pagina escolhida, evitando salvar mais de uma vez no BD
+			res.redirect('/noticias');
+			//este comando fazia o salvar dos dados
+			// res.render("noticias/noticias", {noticias : result})
+		});
+
 	});	
 
 };
